a4e26807b20d15ee418aaf273baca6dc28878967
mateuswetah <mateus.m.luna@gmail.com>
2021-10-21 12:02:35 -0300

    Repository level values to list and table view modes

--- tainacan/view-mode-filefestivallist.php
+++ tainacan/view-mode-filefestivallist.php
@@ -1,35 +1,60 @@
 <?php if ( have_posts() ) : ?>
+	<?php 
+		$is_repository_level = !isset($request['collection_id']);
+		$has_thumbnail_enabled = in_array('thumbnail', $view_mode_displayed_metadata);
+		$has_title_enabled = in_array('title', $view_mode_displayed_metadata);
+		$has_description_enabled = in_array('description', $view_mode_displayed_metadata);
+		$has_meta = isset($view_mode_displayed_metadata['meta']) && count($view_mode_displayed_metadata['meta']) > 0;
+	?>
 	<ul class="tainacan-filefestival-list-container">
 
 		<?php while ( have_posts() ) : the_post(); ?>
-			
 			<li class="tainacan-filefestival-list-item">
-				<?php if ( has_post_thumbnail() ) : ?>
-					<a 
-							href="<?php the_permalink(); ?>"
-							style="background-image: url(<?php the_post_thumbnail_url( 'tainacan-medium' ) ?>)"
-							class="filefestival-list-item-thumbnail">
-						<?php the_post_thumbnail( 'tainacan-medium' ); ?>
-						<div class="skeleton"></div> 
-					</a>
-				<?php else : ?>
-					<a 
-							href="<?php the_permalink(); ?>"
-							class="filefestival-list-item-thumbnail">
-						<?php echo '<img alt="', esc_attr_e('Minatura da imagem do item', 'filefestival'), '" src="', esc_url(get_stylesheet_directory_uri()), '/images/thumbnail_placeholder.png">'?>
-						<div class="skeleton"></div> 
-					</a>
+
+				<?php if ( $has_thumbnail_enabled ) : ?>
+					<?php if ( has_post_thumbnail() ) : ?>
+						<a 
+								href="<?php the_permalink(); ?>"
+								style="background-image: url(<?php the_post_thumbnail_url( 'tainacan-medium' ) ?>)"
+								class="filefestival-list-item-thumbnail">
+							<?php the_post_thumbnail( 'tainacan-medium' ); ?>
+							<div class="skeleton"></div> 
+						</a>
+					<?php else : ?>
+						<a 
+								href="<?php the_permalink(); ?>"
+								class="filefestival-list-item-thumbnail">
+							<?php echo '<img alt="', esc_attr_e('Minatura da imagem do item', 'filefestival'), '" src="', esc_url(get_stylesheet_directory_uri()), '/images/thumbnail_placeholder.png">'?>
+							<div class="skeleton"></div> 
+						</a>
+					<?php endif; ?>
 				<?php endif; ?>
-				<?php
-					tainacan_the_metadata(
-						array(
-							'metadata__in' => $view_mode_displayed_metadata['meta'],
-							'before_title' => '<h3 class="metadata-label">',
-							'before_value' => '<p class="metadata-value">',
-							'after_title'  => '</h3>',
-							'after_value'  => '</p>'
-						)
-					);
+
+				<?php if ( $is_repository_level && $has_title_enabled ): ?>
+					<div class="metadata-type-core_title">
+						<h3 class="metadata-label"><?php echo __('Título', 'filefestival'); ?></h3>
+						<p class="metadata-value"><?php echo get_the_title(); ?></p>
+					</div>
+				<?php endif; if ( $is_repository_level && $has_description_enabled ): ?>
+					<div class="metadata-type-core_description">
+						<h3 class="metadata-label"><?php echo __('Descrição', 'filefestival'); ?></h3>
+						<p class="metadata-value"><?php echo get_the_excerpt(); ?></p>
+					</div>
+				<?php endif; ?>
+
+				<?php if ( $has_meta ) {
+						tainacan_the_metadata(
+							array(
+								'metadata__in' => $view_mode_displayed_metadata['meta'],
+								'before_title' => '<h3 class="metadata-label">',
+								'before_value' => '<p class="metadata-value">',
+								'after_title'  => '</h3>',
+								'after_value'  => '</p>',
+								'hide_empty'   => false,
+								'empty_value_message' => '<span class="empty-metadata-value">' . __('Informação não fornecida.', 'filefestival') . '</span>'
+							)
+						);
+					}
 				?>
 			</li>
 		
--- tainacan/view-mode-filefestivaltable.php
+++ tainacan/view-mode-filefestivaltable.php
@@ -1,28 +1,65 @@
 <?php if ( have_posts() ) : ?>
 	<?php 
-		$metadata_labels = [];
 		$metadata_repository = \Tainacan\Repositories\Metadata::get_instance();
+		$metadata_objects = [];
+		$is_repository_level = !isset($request['collection_id']);
+		$has_title_enabled = in_array('title', $view_mode_displayed_metadata);
+		$has_description_enabled = in_array('description', $view_mode_displayed_metadata);
+		$has_meta = isset($view_mode_displayed_metadata['meta']) && count($view_mode_displayed_metadata['meta']) > 0;
 
-		$metadata_objects = $metadata_repository->fetch([ 'post__in' => $view_mode_displayed_metadata['meta'] ],	'OBJECT');
-		
-		$metadata_labels = array_map(function($metadatum_object) {
-			return $metadatum_object->get_name();
-		}, $metadata_objects);
+		if ( $has_meta ) {
+			if ( !$is_repository_level ) {
+				$collection = tainacan_get_collection([ 'collection_id' => $request['collection_id'] ]);
+				$metadata_objects = $metadata_repository->fetch_by_collection(
+					$collection,
+					[ 'post__in' => $view_mode_displayed_metadata['meta'] ],
+					'OBJECT'
+				);
+			} else {
+				$metadata_objects = $metadata_repository->fetch(
+					[ 
+						'post__in' => $view_mode_displayed_metadata['meta'],
+						'meta_query' => [
+							[
+								'key'     => 'collection_id',
+								'value'   => 'default',
+								'compare' => '='
+							]
+						],
+						'include_control_metadata_types' => true
+					],
+					'OBJECT'
+				);
+			}
+		}
 	?>
 	<div class="tainacan-filefestival-table-container">
 		<table class="tainacan-filefestival-table-container-table">
 			<thead>
 				<tr>
-					<?php foreach($metadata_labels as $metadatum) { echo '<th>' . $metadatum . '</th>'; } ?>
+					<?php if ( $is_repository_level ): ?>
+						<?php if ( $has_title_enabled ): ?>
+							<th class="Tainacan\Metadata_Types\Core_Title"><?php echo __('Título', 'filefestival'); ?></th>
+						<?php endif; if ( $has_description_enabled ) :?>
+							<th class="Tainacan\Metadata_Types\Core_Description"><?php echo __('Descrição', 'filefestival'); ?></th>
+						<?php endif; ?>
+					<?php endif; ?>
+					<?php foreach($metadata_objects as $metadatum): ?>
+						<th class="<?php echo $metadatum->get_metadata_type(); ?>"><?php echo $metadatum->get_name(); ?> </th>
+					<?php endforeach; ?>
 				</tr>
 			</thead>
 			<tbody>
 			<?php while ( have_posts() ) : the_post(); ?>
 				
 				<tr class="tainacan-filefestival-table-item">
-					<?php
-						tainacan_the_metadata(
-							array(
+					<?php if ( $is_repository_level && $has_title_enabled ): ?>
+						<td class="metadata-type-core_title"><p class="metadata-value"><?php echo get_the_title(); ?></p></td>
+					<?php endif; if ( $is_repository_level && $has_description_enabled ): ?>
+						<td class="metadata-type-core_description"><p class="metadata-value"><?php echo get_the_excerpt(); ?></p></td>
+					<?php endif; ?>
+					<?php if ( $has_meta ) {
+							tainacan_the_metadata(array(
 								'metadata__in' => $view_mode_displayed_metadata['meta'],
 								'before_title' => '<h3 class="metadata-label">',
 								'before_value' => '<p class="metadata-value">',
@@ -30,8 +67,8 @@
 								'after_value'  => '</p>',
 								'before' => '<td class="metadata-type-$type" $id>',
 								'after' => '</td>'
-							)
-						);
+							));
+						} 
 					?>
 				</tr>
 			
