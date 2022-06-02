f60e66aed5df5fa4207c883313d2932ad5e6c2f6
mateuswetah <mateus.m.luna@gmail.com>
2022-06-02 10:40:14 -0300

    Adds collections links.

--- tainacan/archive-items.php
+++ tainacan/archive-items.php
@@ -1,13 +1,49 @@
-<?php get_header(); ?>
+<?php	
+	get_header();
+	
+	$Tainacan_Collections = \Tainacan\Repositories\Collections::get_instance();
 
-<div class="entry-content">
+	$collections = $Tainacan_Collections->fetch(
+		array(
+			'posts_per_page' => 6,
+			'post__not_in' => [ tainacan_get_collection_id() ] //'exclude' => [ tainacan_get_collection_id() ]
+		)
+	, 'OBJECT' );
 
-    <section class="alignwide">
+	if ( is_array($collections) && count($collections) >= 0 ) {
+	
+		$collections_links_html = '';
 
+		ob_start();
+		?>
+		<nav>
+			<ul class="tainacan-collections-links">
+				<?php foreach($collections as $collection) : ?>
+					<li><a href="<?php echo $collection->get_url(); ?>"><?php echo $collection->get_name(); ?></a></li>
+				<?php endforeach; ?>
+			</ul>
+		</nav>
 		<div class="tainacan-items-list-heading">
 			<h1><?php tainacan_the_collection_name(); ?></h1>
 			<p><?php tainacan_the_collection_description(); ?></p>
 		</div>
+		<?php
+
+		$collections_links_html = ob_get_contents();
+		ob_clean();
+
+		if ( !empty($collections_links_html) ) {
+			echo '<script type="text/javascript">
+					wp.hooks.addFilter("tainacan_faceted_search_collection_' . tainacan_get_collection_id() . '_search_control_before", "tainacan-hooks", function() { return `' . $collections_links_html . '`; });
+				</script>';
+		}
+	}
+	
+?>
+
+<div class="entry-content">
+
+    <section class="alignwide">
 		<?php 
 			tainacan_the_faceted_search([
 				'enabled_view_modes' => [ 'filefestivalgrid', 'filefestivalgrid2', 'filefestivallist', 'filefestivaltable' ],
